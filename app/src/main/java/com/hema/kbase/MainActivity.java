c5e27339fb0b27658c15f7720db5b59b6708bfec
xpk <xingpeikai0531@163.com>
2016-12-29 20:17:54 +0800

    Initial commit

---
+++ app/src/main/java/com/hema/kbase/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hema.kbase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
